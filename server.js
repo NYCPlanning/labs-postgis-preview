9068b32dc9968aaa7e768888233e519132337b81
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2016-05-28 05:30:34 +0100

    Update server.js
    1. removing repeated error logging for the same errors;
    2. must reject when an error occurs;
    3. removing useless condition for the `delete` operation
    4. making code shorter

--- server.js
+++ server.js
@@ -3,57 +3,51 @@
 
 //dependencies
 var express = require('express'),
-  Mustache = require('mustache'),
-  pgp = require('pg-promise')(),
-  dbgeo = require('dbgeo'),
-  jsonexport = require('jsonexport');
+    Mustache = require('mustache'),
+    pgp = require('pg-promise')(),
+    dbgeo = require('dbgeo'),
+    jsonexport = require('jsonexport');
 
 //create express app and prepare db connection
 var app = express(),
-  port = process.env.PORT || 4000,
-  config = require('./config.js'),
-  db = pgp(config);
+    port = process.env.PORT || 4000,
+    config = require('./config.js'),
+    db = pgp(config);
 
 //use express static to serve up the frontend
 app.use(express.static(__dirname + '/public'));
 
 //expose sql endpoint, grab query as URL parameter and send it to the database
-app.get('/sql', function(req, res){
-  var sql = req.query.q;
-  var format = req.query.format || 'topojson';
-  console.log('Executing SQL: ' + sql, format);
+app.get('/sql', function (req, res) {
+    var sql = req.query.q;
+    var format = req.query.format || 'topojson';
+    console.log('Executing SQL: ' + sql, format);
 
-  //query using pg-promise
-  db.any(sql)
-    .then(function (data) { //use dbgeo to convert WKB from PostGIS into topojson
-        if(format=='csv') {
-          return jsonExport(data);
-        } else {
-          return dbGeoParse(data, format);
-        }
-       
-    })
-    .then(function (data) {
-      if(format=='csv'){
-        res.setHeader('Content-disposition', 'attachment; filename=query.csv');
-        res.setHeader('Content-Type','text/csv');
-        res.send(data);
-      } else if (format=='geojson') {
-        res.setHeader('Content-disposition', 'attachment; filename=query.geojson');
-        res.setHeader('Content-Type','application/json');
-        res.send(data);
-      } else {
-        res.send(data);
-      }
-        
-    })
-    .catch(function (err) { //send the error message if the query didn't work
-        var msg = err.message || err;
-        console.log("ERROR:", msg);
-        res.send({
-            error: msg
+    //query using pg-promise
+    db.any(sql)
+        .then(function (data) { //use dbgeo to convert WKB from PostGIS into topojson
+            return format == 'csv' ? jsonExport(data) : dbGeoParse(data, format);
+        })
+        .then(function (data) {
+            if (format == 'csv') {
+                res.setHeader('Content-disposition', 'attachment; filename=query.csv');
+                res.setHeader('Content-Type', 'text/csv');
+                res.send(data);
+            } else if (format == 'geojson') {
+                res.setHeader('Content-disposition', 'attachment; filename=query.geojson');
+                res.setHeader('Content-Type', 'application/json');
+                res.send(data);
+            } else {
+                res.send(data);
+            }
+        })
+        .catch(function (err) { //send the error message if the query didn't work
+            var msg = err.message || err;
+            console.log("ERROR:", msg);
+            res.send({
+                error: msg
+            });
         });
-    });
 
 });
 
@@ -75,17 +69,20 @@ function dbGeoParse(data, format) {
 }
 
 function jsonExport(data) {
-  //remove geom
-  data.forEach(function(row) {
-    (row.geom) ? delete row.geom : null;
-  });
-
-  return new Promise(function (resolve, reject) {
-      jsonexport(data,function(err, csv){
-      if(err) return console.log(err);
-      resolve(csv);
+    //remove geom
+    data.forEach(function (row) {
+        delete row.geom;
     });
-  });
+    
+    return new Promise(function (resolve, reject) {
+        jsonexport(data, function (err, csv) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(csv);
+            }
+        });
+    });
 }
 
 //start the server
